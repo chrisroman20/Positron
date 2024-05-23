c4763fb008bbc4dcb34b214970c3add5ee38621f
NOMBRE <l20211837@tectijuana.edu.mx>
2024-05-23 12:11:24 -0700

    Base

---
+++ WpfAppMahAppsIconPacks/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+using System.Windows.Controls;
+using Positron.UserControls;
+
+namespace Positron
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cbSistemas.SelectionChanged += cbSistemas_SelectionChanged;
+        }
+
+        private void cbSistemas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedSystem = (cbSistemas.SelectedItem as ComboBoxItem).Content.ToString();
+            contentControlSistema.Content = null;
+            sistemaTitulo.Text = selectedSystem;
+
+
+            switch (selectedSystem) {
+                case "Sistema tegumentario":
+                    ucSistemaTegumentario sistemaTegumentario = new ucSistemaTegumentario();
+                    sistemaTegumentario.PartSelected += SistemaPartSelected;
+                    contentControlSistema.Content = sistemaTegumentario;
+                    break;
+                case "Sistema urinario":
+                    ucSistemaUrinario sistemaUrinario = new ucSistemaUrinario();
+                    sistemaUrinario.PartSelected += SistemaPartSelected;
+                    contentControlSistema.Content = sistemaUrinario;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void SistemaPartSelected(object sender, string part)
+        {
+            if (!lbPartes.Items.Contains(part))
+            {
+                lbPartes.Items.Add(part);
+            }
+        }
+    }
+}
---
+++ WpfAppMahAppsIconPacks/UserControls/ucSistemaUrinario.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Positron.UserControls
+{
+    public partial class ucSistemaUrinario : UserControl
+    {
+        public ucSistemaUrinario()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Label selectedLabel = sender as Label;
+            if (selectedLabel != null)
+            {
+                string selectedPart = selectedLabel.Tag.ToString();
+                OnPartSelected(selectedPart);
+            }
+        }
+
+        public delegate void PartSelectedEventHandler(object sender, string part);
+        public event PartSelectedEventHandler PartSelected;
+
+        protected virtual void OnPartSelected(string part)
+        {
+            PartSelected?.Invoke(this, part);
+        }
+    }
+}
